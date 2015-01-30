8b8569095ff4cefe6c9c07dfdd90f9715d5adf93
Bruce Crevensten <bruce.crevensten@gmail.com>
2015-01-29 15:33:08 -0900

    Initial import of CentOS6.5 manifests, need to reconcile

--- manifests/postinstall.pp
+++ manifests/postinstall.pp
@@ -1,21 +1,39 @@
-# == Class ckan::postinstall
-#
-# Manages tasks to be performed after the initial install like
-# initializing the database
-#
-# details: http://docs.ckan.org/en/ckan-2.0/install-from-package.html
-#
 class ckan::postinstall {
-  include check_run
 
-  check_run::task { 'init_db':
-    exec_command => '/usr/bin/ckan db init',
+  exec { 'init_db':
+	command => '/usr/lib/ckan/default/bin/paster db init -c /etc/ckan/default/production.ini',
+	cwd => '/usr/lib/ckan/default/src/ckan',
+	user => 'ckan',
   }
 
-  check_run::task { 'set_database_perms':
-    exec_command => "/usr/bin/python /usr/lib/ckan/default/src/ckan/ckanext/\
-datastore/bin/datastore_setup.py ckan_default datastore_default ckan_default\
- ckan_default datastore_default -p postgres",
-    require      => Check_run::Task['init_db'],
+  file { '/etc/ckan/default/who.ini':
+     ensure => 'link',
+     target => '/usr/lib/ckan/default/src/ckan/who.ini',
   }
+
+  # Add extensions
+  class { 'ckan::ext::harvest':
+    require => Exec['init_db']
+  } ->
+  class{ 'ckan::ext::spatial':
+      require => Exec['init_db'],
+  } ->
+  # This is a sort of hinky way to do this, but we can't have the harvest/spatial extensions here during the initial buildout.
+  # Would be better to learn to use Augeas to drop in the missing sections instead of rewriting the plugin list.
+  file_line { 'enable plugins in config file':
+    path => '/etc/ckan/default/production.ini',
+    match => '^ckan\.plugins.*$',
+    line => 'ckan.plugins = stats text_preview recline_preview datastore resource_proxy pdf_preview harvest csw_harvester spatial_metadata'
+  } ->
+  # Configures the harvester to run.
+  file { '/etc/supervisord.conf':
+    source => 'puppet:///modules/ckan/supervisord.conf'
+  } -> cron { 'harvest':
+    command => '/usr/lib/ckan/default/bin/paster --plugin=ckanext-harvest harvester run --config=/etc/ckan/default/production.ini',
+    user => 'ckan',
+    minute => '*/1',
+  }
+
+  #include ckan::ext::googleanalytics
+
 }
